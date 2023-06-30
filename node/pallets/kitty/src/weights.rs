a32dbf900e5d1f608a11d7a2058624b9078f7ec8
Jahangir Anwari <jahangir@tarkalabs.com>
2023-06-30 11:55:27 -0300

    Fix weights for Kitty actions - This is to prevent runtime panic

--- node/pallets/kitty/src/weights.rs
+++ node/pallets/kitty/src/weights.rs
@@ -17,37 +17,45 @@ pub trait WeightInfo {
 pub struct SubstrateWeight<T>(PhantomData<T>);
 impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
 	fn create_kitty() -> Weight {
-		todo!()
+		Weight::from_parts(9_000_000, 0)
+			.saturating_add(T::DbWeight::get().writes(1_u64))
 	}
 
 	fn transfer() -> Weight {
-		todo!()
+		Weight::from_parts(9_000_000, 0)
+			.saturating_add(T::DbWeight::get().writes(1_u64))
 	}
 
 	fn set_price() -> Weight {
-		todo!()
+		Weight::from_parts(9_000_000, 0)
+			.saturating_add(T::DbWeight::get().writes(1_u64))
 	}
 
 	fn buy_kitty() -> Weight {
-		todo!()
+		Weight::from_parts(9_000_000, 0)
+			.saturating_add(T::DbWeight::get().writes(1_u64))
 	}
 }
 
 // For backwards compatibility and tests
 impl WeightInfo for () {
 	fn create_kitty() -> Weight {
-		todo!()
+		Weight::from_parts(9_000_000, 0)
+			.saturating_add(RocksDbWeight::get().writes(1_u64))
 	}
 
 	fn transfer() -> Weight {
-		todo!()
+		Weight::from_parts(9_000_000, 0)
+			.saturating_add(RocksDbWeight::get().writes(1_u64))
 	}
 
 	fn set_price() -> Weight {
-		todo!()
+		Weight::from_parts(9_000_000, 0)
+			.saturating_add(RocksDbWeight::get().writes(1_u64))
 	}
 
 	fn buy_kitty() -> Weight {
-		todo!()
+		Weight::from_parts(9_000_000, 0)
+			.saturating_add(RocksDbWeight::get().writes(1_u64))
 	}
 }
